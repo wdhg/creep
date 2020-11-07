2fcab7351bef3bf20cd0505fe33e4cabb883d168
wdhg <wdhgrant@gmail.com>
2020-11-07 23:36:22 +0000

    move flags inside main

--- main.go
+++ main.go
@@ -6,17 +6,14 @@ import (
 	"time"
 )
 
-var (
-	help        = flag.Bool("h", false, "Show help")
-	start       = flag.String("s", "https://news.ycombinator.com", "The site to start crawling from")
-	maxCount    = flag.Int("n", 100, "Number of urls to scrape")
-	threadCount = flag.Int("tc", 10, "Number of threads")
-	timeout     = flag.Int64("t", 5000, "Timeout for each http request (ms)")
-	logging     = flag.Bool("l", false, "Enables logging")
-	output      = flag.String("o", "", "Name of output file to write to. If not set will output to terminal")
-)
-
 func main() {
+	help := flag.Bool("h", false, "Show help")
+	start := flag.String("s", "https://news.ycombinator.com", "The site to start crawling from")
+	maxCount := flag.Int("n", 100, "Number of urls to scrape")
+	threadCount := flag.Int("tc", 10, "Number of threads")
+	timeout := flag.Int64("t", 5000, "Timeout for each http request (ms)")
+	logging := flag.Bool("l", false, "Enables logging")
+	output := flag.String("o", "", "Name of output file to write to. If not set will output to terminal")
 	flag.Parse()
 	if *help {
 		flag.Usage()
